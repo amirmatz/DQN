1dbd3d91523d2bd6259408b9b0182f1ce5676a53
Yoav Halperin <yoav.halperin@planckresolution.com>
2020-04-14 14:27:35 +0300

    Added word2vec

---
+++ vectorize_words.py
@@ -0,0 +1,18 @@
+import gzip
+import json
+import numpy as np
+
+
+class Word2Vec:
+    def __init__(self) -> None:
+        with gzip.open("word2vec.json.gz") as f:
+            self._d = json.load(f)
+
+        for k, v in self._d.items():
+            self._d[k] = np.array(v)
+
+    def __getitem__(self, item):
+        if item in self._d:
+            return self._d[item]
+
+        raise NotImplementedError("What to do if not found?")  # TODO: implement
