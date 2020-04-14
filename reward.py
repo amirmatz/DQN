dce1e66014f7abcbd927a61b086ce4fdecd0cdf1
amirmatz <ramatz777@gmail.com>
2020-04-14 23:25:40 +0300

    add reward

--- reward.py
+++ reward.py
@@ -1,3 +1,8 @@
+from nltk.translate.bleu_score import sentence_bleu
+
+REWARD_MULTIPLIER = 100
+POSITIVE_REWARD_THRESHOLD = 0.75
+
 
 def bleu_reward(sent1, sent2):
-    pass
+    return REWARD_MULTIPLIER(sentence_bleu(sent1, sent2) - POSITIVE_REWARD_THRESHOLD)
