9f73c433dc987bd929941f782e50948a91206dfa
amirmatz <ramatz777@gmail.com>
2020-04-15 16:19:32 +0300

    fixed reward

--- reward.py
+++ reward.py
@@ -5,4 +5,4 @@ POSITIVE_REWARD_THRESHOLD = 0.75
 
 
 def bleu_reward(sent1, sent2):
-    return REWARD_MULTIPLIER(sentence_bleu(sent1, sent2) - POSITIVE_REWARD_THRESHOLD)
+    return REWARD_MULTIPLIER*(sentence_bleu(sent1, sent2) - POSITIVE_REWARD_THRESHOLD)
